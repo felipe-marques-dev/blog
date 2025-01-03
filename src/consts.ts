3112848c48d051d93ed19ee5fd86f10570b79230
Felipe <felipemarques.computacao@gmail.com>
2025-01-03 12:35:08 -0300

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '<Felipersas/>';
+export const SITE_DESCRIPTION = 'Felipersas Dev - Blog pessoal sobre desenvolvimento de software e tecnologia.';
+export const GITHUB_URL = 'https://github.com/felipe-marques-dev';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/felipe-marques-a748b9299?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app';
